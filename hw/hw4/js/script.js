2d1ba03d4d2ef5f0ef9e157a0be952839219cb15
vkoschman <207955@niuitmo.ru>
2020-08-14 14:24:24 +0300

    hw4 - 1st part finished

--- hw/hw4/js/script.js
+++ hw/hw4/js/script.js
@@ -1,22 +1,20 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
+const width_second = window.innerWidth;
+const height_second = window.innerHeight;
 const sliderHeight = 100;
 const colors = ['#d73027', '#1a9850']; // цвета градиента
 const noColor = '#CDCCCC'; // для стран, по которым нет данных 
 const duration = 700;// продолжительность анимации
 
-loadData().then(({data, world})=>{
+loadData().then(({data, world}) => {
     // переменные состояния с начальными значениями
     let year = '1900';
     let param = 'gdp';
 
-    // основной svg-элемент, содержащий карту
+    // основной svg-элемент, содержащий карту          
     const svg = d3.select('body').append('svg')
-                .attr('width', width)
-                .attr('height', height);
-
+    
     // TODO Part 1: тут можно создать линейную цветовую шкалу на основе цветов
-    //  const color = ...
+    const color = d3.scaleLinear().range(colors);
     
     // массив с годами
     const years = Object.keys(data[0][param])
@@ -25,7 +23,7 @@ loadData().then(({data, world})=>{
 
     // TODO Part 2: тут можно создать шкалы х и y
     // ...
-    //const x = d3.scaleBand(years, [0, width]).padding(.1);
+    // const x = d3.scaleBand(years, [0, width_second]).padding(.1);
     // y = d3.scaleLinear().range([sliderHeight, 0]);
     
     // TODO Part 3: тут можно создать svg внутри #menu и добавить туда легенду
@@ -46,29 +44,29 @@ loadData().then(({data, world})=>{
     //     .style("fill", "url(#gradient)");
 
     // const axis = legend.append('g').attr('id','axis').attr('transform', 'translate(0,20)');
-
+     
     // вызываем update() при изменении param
-    d3.selectAll('input[name="param"]').on('change',function(){
-        param = this.value;
-        update();
-    })
+     d3.selectAll('input[name="param"]').on('change',function(){
+      param = this.value;
+      update();
+    });
 
     // создаем слайдер
     const slider = d3.sliderBottom()
         .domain([d3.min(years),d3.max(years)])
-        .step(1).width(width).default(year)
+        .step(1).width(width_second).default(year)
         .ticks(0).displayValue(false)
         .on('onchange', val => { // вызываем update() при изменении year
             year = val;
             update();
         });
-        
+
     // создаем svg для добавления своих элементов
     const range = d3.select('#slider').append('svg')
-        .attr('width', width).attr('height', sliderHeight);
-    
+                    .attr('width', width_second).attr('height', sliderHeight);
+
     // накидываем overlay
-    range.append('rect').attr('width', width).attr('height', height).attr('id', 'overlay');
+    range.append('rect').attr('width', width_second).attr('height', height_second).attr('id', 'overlay');
     
     // создаем контейнер для барчарта
     const chart = range.append('g');
@@ -78,51 +76,75 @@ loadData().then(({data, world})=>{
     
     // убираем лишние элементы и делаем кастюмный thumb
     d3.select('g.slider').selectAll('line').remove();
-    d3.select('g.parameter-value path').attr('d', `M 0,0 V ${sliderHeight} H -${x.bandwidth()} V 0 Z`);
-
+    // d3.select('g.parameter-value path').attr('d', `M 0,0 V ${sliderHeight} H -${x.bandwidth()} V 0 Z`);
+    
     // TODO Part 1: тут можно создать проекцию d3.geoEquirectangular() и подстроить ее под размеры экрана, а также
     // создать генератор пути
-    // ...
-
     // TODO Part 1: тут можно добавить карту мира на основе генератора пути
-    // const map = ...
+    let map = svg.attr('id', 'map')
+    var projection = d3.geoEquirectangular();
+    var geoGenerator = d3.geoPath().projection(projection);
+    const { height, width } = document.getElementById('map').getBoundingClientRect()
+    projection.fitExtent([[ 0, 0 ], [width, height]], world)
     
-    // TODO Part 1: добавить обработчики событий мыши для отображения имени страны в элементе #country
-    // ...
+    map = map.selectAll('path')
+          .data(world.features)
+          .enter()
+          .append('path')
+          .attr('d', geoGenerator);
+    
+    map.on('mouseover', overCountry)   //  TODO Part 1: добавить обработчики событий мыши для отображения имени страны в элементе #country
+        .on('mouseout', outOfCountry);
+        //   .attr('stroke', 'white');
+
+    function overCountry(d){
+        console.log(d.properties.name)
+        d3.select('#country').text(d.properties.name);
+    }
+
+    function outOfCountry(){
+        d3.select('#country').text(null);
+    }
 
     // эта функция будет обновлять оба графика при изменении какого-либо из двух основных параметров
     function update(){
-        // обновляем год
-        d3.select('#year').text(year);
+      // обновляем год
+      d3.select('#year').text(year);
 
-        // обновляем домен цветовой шкалы
-        //color.domain(d3.extent(data.map(d=>+d[param][year])));
+      // обновляем домен цветовой шкалы
+      color.domain(d3.extent(data.map(d => +d[param][year])));
+      let colorMap = d => +d[param][year];
 
-        // изменяем цвета стран
-        // map.attr('fill', feature =>{
-        //         // находим страну в наборе данных
-        //         let country = data.find(d=>d.geo === feature.id.toLowerCase());
-        //         // TODO Part 1: задать цвет страны в зависимости от выбранных параметров
-        //         // ...
-        //     });
+      // изменяем цвета стран
+      map.attr('fill', feature => {
+            // находим страну в наборе данных
+            let country = data.find(d => d.geo === feature.id.toLowerCase());
+            // TODO Part 1: задать цвет страны в зависимости от выбранных параметров
+            if (country == undefined) {
+                return noColor;
+            } else {
+                return color(colorMap(country));
+            };
+        });
 
-        // берем набор значений нужного показателя для каждого из годов для барчарта
-        let array = years.map(y=>d3.sum(data.map(d=>+d[param][y])));
-        // TODO Part 2: обновить домен шкалы y
-        // ...
+      // берем набор значений нужного показателя для каждого из годов для барчарта
+      let array = years.map(y => d3.sum(data.map(d => +d[param][y])));
+      // TODO Part 2: обновить домен шкалы y
+      // ...
 
-        // TODO Part 2: реализовать создание и добавление барчарта
-        // ...
+      // TODO Part 2: реализовать создание и добавление барчарта
+      // ...
 
-        
-        // TODO Part 3: обновляем шкалу в легенде на основе выбранных параметров
-        // ...
+      
+      // TODO Part 3: обновляем шкалу в легенде на основе выбранных параметров
+      // ...
     }
-    // вызываем update() при инициации
-    update();
-});
 
-async function loadData() {
+  // вызываем update() при инициации
+  update();
+  });
+
+  async function loadData() {
     const population = await d3.csv('data/pop.csv');
     const rest = { 
         'gdp': await d3.csv('data/gdppc.csv'),
